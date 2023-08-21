0e2bcaa171a8c78fe863320d8b1f1e0860c492de
Thayan <thayan.raveendran@empoweredsystems.com>
2023-08-21 22:17:54 +0100

    Step2: Changes based on code review, tidy code, add dependency injection

--- src/main/java/com/FreePark/controllers/ParkingController.java
+++ src/main/java/com/FreePark/controllers/ParkingController.java
@@ -1,8 +1,10 @@
-package com.FreePark;
+package com.FreePark.controllers;
 
 import com.FreePark.service.ParkingServiceImpl;
 import com.FreePark.dto.ParkingDTO;
 import com.FreePark.service.ParkingService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ParkingController {
 
-    @RequestMapping(value = "/parking", method = RequestMethod.GET)
+    ParkingService parkingService;
+    public ParkingController(ParkingService parkingService)
+    {
+        this.parkingService = parkingService;
+    }
+
+    @GetMapping(value = "/parking/{location}")
     @ResponseBody
     public List<ParkingDTO> getLocation(@PathVariable(value = "location") String location)
     {
-        ParkingService parkingService = new ParkingServiceImpl();
-        return parkingService.getParkingSpots(location);
+        return this.parkingService.getParkingSpots(location);
     }
 }
